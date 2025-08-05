9f2dc0b2a53ca9c666d45eb193629cbf7269912d
Janset-E <janset.edie@gmail.com>
2025-08-05 12:50:06 +0300

    Add /json route to return JSON message

--- myApp.js
+++ myApp.js
@@ -7,9 +7,11 @@ let app = express();
  res.sendFile(absolutePath);
         });
 
-app.use('/public', express.static(__dirname+'/public'));
+ app.use('/public', express.static(__dirname+'/public'));
 
-
+ app.get("/json",(req,res)=> {
+ res.json({ message:"Hello json"});
+ });
 
 
 
